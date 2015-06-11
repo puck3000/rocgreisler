23206283ad935b532851d90b47123bd44763ad39
puck3000 <daniel.lerch@gmail.com>
2015-06-11 22:38:19 +0200

    Updates

--- app/gulp/tasks/browser-sync.js
+++
@@ -1,8 +0,0 @@
-var gulp = require('gulp');
-var browserSync = require('browser-sync');
-var config     = require('../config').browserSync;
-
-// browser-sync task for starting the server.
-gulp.task('browser-sync', function() {
-    browserSync(config);
-});
--- app/gulp/tasks/deploy.js
+++
@@ -1,9 +0,0 @@
-var gulp = require('gulp');
-var ghpages = require('gh-pages');
-var path = require('path');
-
-
-// deploy the dist folder to gh-pages
-gulp.task("deploy", function(cb) {
-  ghpages.publish(path.join(process.cwd(), "build"), cb);
-});	
